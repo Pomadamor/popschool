58d70bd693d65a625a46309d655d507e4333a660
Ines Lerna <lerna@MacBook-Pro-de-Ines.local>
2022-03-04 13:18:40 +0100

    ajout de l'enregistrement des formations

--- src/pages/Formulaire.vue
+++ src/pages/Formulaire.vue
@@ -1,5 +1,59 @@
-  <template>
-    <div class="formulaire">
-        <h1>Formulaire</h1>
+<template>
+  <div class="formulaire">
+  <h1>Formulaire</h1>
+    <div>
+      <h2>Formation</h2>
+      <p>Nom de la formation</p>
+      <input v-model="formationTabsNews.nomFormation" />
+      <br>
+      <p>Date de début</p>
+      <input v-model="formationTabsNews.dateDebutFormation" />
+      <br>
+      <p>Date de fin</p>
+      <input v-model="formationTabsNews.dateFinFormation" />
+      <br>
+      <button @click="saveFormation()">Enregistrer</button>
     </div>
-  </template>
+    <table>
+      <thead>
+          <tr>
+              <th>Nom de la formation </th>
+              <th>Début de la formation </th>
+              <th>Fin de la formation </th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr v-for="formationTab in formationTabs" :key="formationTab">
+            <td>{{formationTab.nomFormation}}</td>
+            <td>{{formationTab.dateDebutFormation}}</td>
+            <td>{{formationTab.dateFinFormation}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+          return {
+            formationTabsNews: [
+              {nomFormation: "", dateDebutFormation: '', dateFinFormation: ''},
+            ],
+            formationTabs: [
+              {nomFormation: "Chandler Bing", dateDebutFormation: '42/42/42', dateFinFormation: '42/42/42'},
+            ]
+          }
+        },
+
+      methods: {
+        saveFormation: function () {
+          this.formationTabs.push(this.formationTabsNews);
+          alert('vous avez une nouvelle formation', this.formationTabs)
+
+    },
+  },
+}
+</script>
+<style>
+</style>
+  
